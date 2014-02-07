078e02ead7af7dadb35ad9cdbbbbb4457a5c3995
hmcgowan <hugh_mcgowan@yahoo.com>
2014-02-07 11:38:02 -0600

    display configuration in logging

--- src/main/java/com/e2open/falcon/framework/Configuration.java
+++ src/main/java/com/e2open/falcon/framework/Configuration.java
@@ -1,9 +1,12 @@
 package com.e2open.falcon.framework;
 
+import org.apache.log4j.Logger;
+
 import java.io.IOException;
 import java.util.Properties;
 
 public final class Configuration {
+    private static final Logger log = Logger.getLogger(Configuration.class);
     private static final Properties properties = new Properties();
 
     static {
@@ -13,6 +16,7 @@ public final class Configuration {
         if (localConfiguration != null) {
             loadPropertiesFromFile(loader, localConfiguration);
         }
+        showConfiguration();
     }
 
     private static void loadPropertiesFromFile(ClassLoader loader, String fileName) {
@@ -26,4 +30,13 @@ public final class Configuration {
     public static String getProperty(String key) {
         return properties.getProperty(key);
     }
+
+    private static void showConfiguration() {
+        log.info("===================================================================================");
+        for (String property : properties.stringPropertyNames()) {
+            log.info(String.format("%-40s%s", property, getProperty(property)));
+        }
+        log.info("===================================================================================");
+    }
+
 }
